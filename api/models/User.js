5abd2f9b8d5bfda6aafc259bbcf1a5ad024b72d4
Arjun Sharma <arjunsharma6622@gmail.com>
2023-10-14 17:57:14 +0530

    user phone validation added

--- api/models/User.js
+++ api/models/User.js
@@ -1,36 +1,3 @@
-// const mongoose = require('mongoose');
-
-// const userSchema = new mongoose.Schema({
-//     name: {
-//         type: String,
-//         required: true
-//     },
-//     email: {
-//         type: String,
-//         required: true,
-//         unique: true
-//     },
-//     password: {
-//         type: String,
-//         required: true
-//     },
-//     phone: {
-//         type: String,
-//     },
-//     gender: {
-//         type: String,
-//         enum: ['male', 'female', 'other'],
-//     },
-//     place: {
-//         type: String,
-//     }
-// });
-
-// module.exports = mongoose.model('User', userSchema);
-
-
-
-
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
@@ -41,14 +8,14 @@ const userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
-        unique: true // Ensure that the email field is unique
+        unique: true
     },
     password: {
         type: String,
         required: true
     },
     phone: {
-        type: String, // Keep the phone field without the unique constraint
+        type: String,
     },
     gender: {
         type: String,
@@ -56,20 +23,6 @@ const userSchema = new mongoose.Schema({
     },
     place: {
         type: String,
-    }
-});
-
-// Validate phone uniqueness before saving
-userSchema.pre('save', async function (next) {
-    try {
-        const phoneExists = await this.model('User').exists({ phone: this.phone });
-        if (phoneExists) {
-            const err = new Error('Phone number must be unique');
-            next(err);
-        }
-        next();
-    } catch (error) {
-        next(error);
     }
 });
 
