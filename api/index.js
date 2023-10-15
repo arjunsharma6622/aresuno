b833ced95387b14694269ed22f200552df8babd3
Arjun Sharma <arjunsharma6622@gmail.com>
2023-10-15 20:59:57 +0530

    cors issue resolved, added headers to verce.json file

--- api/index.js
+++ api/index.js
@@ -98,21 +98,29 @@ const dotenv = require('dotenv');
 const analytics = require('@vercel/analytics');
 const cookieParser = require('cookie-parser');
 
-// Include your allowCors middleware
-const allowCors = (fn) => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  );
-  if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
-  }
-  return await fn(req, res);
-};
+const allowCors = fn => async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if (req.method === 'OPTIONS') {
+        res.status(200).end()
+        return
+    }
+    return await fn(req, res)
+}
+
+const handler = (req, res) => {
+    const d = new Date()
+    res.end(d.toString())
+}
+
+module.exports = allowCors(handler)
 
 dotenv.config();
 
@@ -121,7 +129,6 @@ const db = process.env.DB_URL;
 const port = process.env.PORT;
 app.use(express.json());
 
-app.use(allowCors); // Use your allowCors middleware here
 app.use(cookieParser());
 
 // Connect to MongoDB database
@@ -154,7 +161,7 @@ app.use((err, req, res, next) => {
 
 // Start server
 app.listen(port, () => {
-    console.log('Server started on port '+ port);
+    console.log('Server started on port ' + port);
 });
 
 analytics.inject();
