05b1b08563daa5ab92099e44161f03e3234e8d6b
Arjun Sharma <arjunsharma6622@gmail.com>
2023-10-15 20:15:09 +0530

    secret key error in secrettoken.js file resolved

--- api/utils/SecretToken.js
+++ api/utils/SecretToken.js
@@ -2,9 +2,12 @@ const dotenv = require('dotenv');
 const jwt = require('jsonwebtoken')
 
 
+const secretKey = process.env.TOKEN_KEY
+
+
 
 module.exports.createSecretToken = (id) => {
-    return jwt.sign({ id }, process.env.TOKEN_KEY, {
+    return jwt.sign({ id }, secretKey, {
       expiresIn: 3 * 24 * 60 * 60,
     });
   };
