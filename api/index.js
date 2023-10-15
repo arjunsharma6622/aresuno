319073f1f5015991b2e73298155ae5e4529fbde1
Arjun Sharma <arjunsharma6622@gmail.com>
2023-10-15 21:30:51 +0530

    cors issue resolved, added headers to verce.json file

--- api/index.js
+++ api/index.js
@@ -4,28 +4,7 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const analytics = require('@vercel/analytics');
 const cookieParser = require('cookie-parser');
-const router = express.Router();
 
-
-
-// CORS handler
-const handleCors = (req, res, next) => {
-    // Set header to allow requests from any origin (value can be different)
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, OPTIONS, DELETE');
-
-    // Preflight CORS handler
-    if (req.method === 'OPTIONS') {
-        return res.status(200).json({ body: 'OK' });
-    }
-
-    next();
-};
-
-// Use the CORS handler in your routes
-router.use(handleCors);
 
 dotenv.config();
 
@@ -39,8 +18,10 @@ app.use(
       origin: "*",
       methods: ["GET", "POST", "PUT", "DELETE"],
       credentials: true,
+      allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Headers'
     })
   );
+
   app.use(cookieParser());
 
 // Connect to MongoDB database
@@ -58,24 +39,7 @@ app.use('/api/business', require('./routes/Business'));
 app.use('/api/user', require('./routes/User'));
 app.use('/api/vendor', require('./routes/Vendor'));
 
-// Error handling middleware for MongoDB duplicate key errors
-app.use((err, req, res, next) => {
-    if (err.name === 'MongoError' && err.code === 11000) {
-        // Duplicate key error
-        const duplicatedKey = err.message.match(/(["'])(\\?.)*?\1/)[0];
-        const message = `Duplicate key error: ${duplicatedKey} already exists.`;
-        console.error('MongoDB Duplicate Key Error:', message);
-        return res.status(400).json({ error: message });
-    }
-    // Handle other errors
-    return res.status(500).json({ error: 'Internal server error' });
-});
-
-
 // Start server
 app.listen(port, () => {
     console.log('Server started on port '+ port);
 });
-
-
-analytics.inject();
