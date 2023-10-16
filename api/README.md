6f71f71faf05a76246b3fc1e73ccf6c796c7f258
Arjun Sharma <arjunsharma6622@gmail.com>
2023-10-16 17:08:41 +0530

    xyz

---
+++ api/README.md
@@ -0,0 +1,1 @@
+# aresuno_server
--- api/index.js
+++ api/index.js
@@ -2,24 +2,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const analytics = require('@vercel/analytics');
 const cookieParser = require('cookie-parser');
 const app = express();
 
 dotenv.config();
 
-
 const db = process.env.DB_URL;
 const port = process.env.PORT;
 app.use(express.json());
 
-app.use(cors({
-    origin: ['http://localhost:5173', 'https://aresuno.vercel.app'],
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
 
-}))
-
+app.use(cors());
 
 app.use(cookieParser());
 
@@ -27,6 +20,12 @@ app.use(cookieParser());
 mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
+
+
+// Start server
+app.listen(port, () => {
+    console.log('Server started on port ' + port);
+});
 
 // Define routes
 app.get('/', (req, res) => {
@@ -38,10 +37,6 @@ app.use('/api/business', require('./routes/Business'));
 app.use('/api/user', require('./routes/User'));
 app.use('/api/vendor', require('./routes/Vendor'));
 
-// Start server
-app.listen(port, () => {
-    console.log('Server started on port ' + port);
-});
 
 
 
