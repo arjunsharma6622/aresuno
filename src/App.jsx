362ff8c730db3f1976ee25951f72ca936a9d6608
Arjun Sharma <arjunsharma6622@gmail.com>
2023-10-22 08:45:42 +0530

    adjusted the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation, useNavigate } from 'react-router-dom';
 import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
 import Hero from './Components/Hero/Hero';
@@ -9,6 +9,8 @@ import VendorDashboard from './Pages/Vendor/Dashboard/VendorDashboard';
 import UserDashboard from './Pages/User/Dashboard/UserDashboard';
 import Login from './Pages/Login/Login';
 import BusinessRegister from './Pages/Business';
+import { useSelector } from "react-redux";
+import { useEffect } from 'react';
 
 function App() {
   return (
@@ -22,6 +24,9 @@ function Main() {
   const location = useLocation();
   const isLoginPage = location.pathname === '/login';
   const isSignpuPage = location.pathname === '/signup';
+  const userType = useSelector((state) => state.user.userType);
+  const navigate = useNavigate()
+
 
   return (
     <>
@@ -29,12 +34,23 @@ function Main() {
       <div className='app'>
         <Routes>
           <Route path='/' element={<Hero />} />
+
           <Route path='/login' element={<Login />} />
+
           <Route path='/signup' element={<Register />} />
-          <Route path='/vendor/register' element={<VendorRegister />} />
-          <Route path='/user/dashboard' element={<UserDashboard />} />
-          <Route path='/vendor/dashboard' element={<VendorDashboard />} />
+
+          {userType === "vendor" && (
+            <Route path="/vendor/dashboard" element={<VendorDashboard />} />
+          )}
+
+          {userType === "user" && (
+            <Route path="/user/dashboard" element={<UserDashboard />} />
+          )}
+
+
+
           <Route path='/contact' element={<h1>Contact</h1>} />
+
           <Route path='/business/register' element={<BusinessRegister />} />
         </Routes>
       </div>
