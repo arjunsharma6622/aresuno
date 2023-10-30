9f3d327d613ed671ebecfbb090f74b1fcc91b374
Arjun Sharma <arjunsharma6622@gmail.com>
2023-10-30 21:58:17 +0530

    profile section update in the vendor dashboard

--- src/Pages/Vendor/Dashboard/VendorDashboard.jsx
+++ src/Pages/Vendor/Dashboard/VendorDashboard.jsx
@@ -26,7 +26,8 @@ import {
     AiOutlineUser,
 } from "react-icons/ai";
 import BusinessEdit from "../../BusinessEdit";
-import { FiEdit, FiX } from "react-icons/fi";
+import { FiEdit, FiEye, FiEyeOff, FiLock, FiX } from "react-icons/fi";
+import { ToastContainer, toast } from "react-toastify";
 
 const Overview = ({ businesses, data }) => {
     return (
@@ -206,14 +207,64 @@ const VendorDashboard = () => {
             amt: 1700,
         },
     ];
-    const [selectedField, setSelectedField] = useState("profile");
+    const [selectedField, setSelectedField] = useState("dashboard");
     const [edit, setEdit] = useState(true);
+    const [updatedPassword, setUpdatedPassword] = useState({
+        newPassword: "",
+        confirmPassword: "",
+    })
+    const [showPassword, setShowPassword] = useState(false);
 
+
+
+    const handlePasswordClick = async (e) => {
+        e.preventDefault();
+
+        try {
+            const res = await axios.patch("https://aresuno-server.vercel.app/api/vendor/", { password: updatedPassword.newPassword }, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem("token")}`,
+                },
+            });
+            setUpdatedPassword({
+                newPassword: "",
+                confirmPassword: "",
+            })
+            console.log(res.data);
+            toast.success("Password Updated");
+        }
+        catch (err) {
+            console.log(err);
+            toast.error("Error Updating Password");
+        }
+    }
 
 
     const handleChange = (e) => {
         setUserEdit({ ...userEdit, [e.target.name]: e.target.value });
-      };
+    };
+
+    const handlePasswordChange = (e) => {
+        setUpdatedPassword({ ...updatedPassword, [e.target.name]: e.target.value });
+    };
+
+    const handleProfileUpdate = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await axios.patch("https://aresuno-server.vercel.app/api/vendor/", {name : userEdit.name}, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem("token")}`,
+                },
+            });
+            console.log(res.data);
+            toast.success("Profile Updated");
+
+        }
+        catch (err) {
+            console.log(err)
+            toast.error("Error Updating Profile");
+        }
+    };
 
     const handleSelectedField = (field) => {
         setSelectedField(field);
@@ -228,7 +279,7 @@ const VendorDashboard = () => {
                 { headers: { Authorization: `Bearer ${token}` } }
             );
             const user = res.data;
-            
+
 
             const resBusinesses = await axios.get(
                 "https://aresuno-server.vercel.app/api/vendor/businesses",
@@ -358,62 +409,155 @@ const VendorDashboard = () => {
                                     <Overview businesses={businesses} data={data} />
                                 )}
                                 {selectedField === "profile" && (
-                                    <div className="width-full">
-                                        <div className="flex justify-between items-center">
-                                            <h2 className="text-lg font-medium">Update Business Details</h2>
-                                            {edit ? (
-                                                <FiEdit
-                                                    className="text-gray-500 cursor-pointer w-6 h-6"
-                                                    onClick={() => {
-                                                        setEdit(!edit);
-                                                    }}
-                                                />
-                                            ) : (
-                                                <FiX
-                                                    className="text-red-500 cursor-pointer w-6 h-6"
-                                                    onClick={() => {
-                                                        setEdit(!edit);
-                                                    }}
-                                                />
-                                            )}
+                                    <div className="w-full flex justify-center items-center flex-col gap-10">
+                                        <img src="https://picsum.photos/200" alt="" className="rounded-full w-28 h-28" />
+
+
+                                        <div className="flex w-full flex-col items-center justify-between gap-8">
+
+                                            <div className="w-1/2">
+                                                <div className="flex justify-between items-center">
+                                                    <h2 className="text-lg font-medium">Update Business Details</h2>
+                                                    {edit ? (
+                                                        <FiEdit
+                                                            className="text-gray-500 cursor-pointer w-6 h-6"
+                                                            onClick={() => {
+                                                                setEdit(!edit);
+                                                            }}
+                                                        />
+                                                    ) : (
+                                                        <FiX
+                                                            className="text-red-500 cursor-pointer w-6 h-6"
+                                                            onClick={() => {
+                                                                setEdit(!edit);
+                                                            }}
+                                                        />
+                                                    )}
+                                                </div>
+
+                                                <div className="">
+                                                    <form className={`flex flex-col gap-4 mt-6`}>
+
+                                                        <input
+                                                            type="text"
+                                                            value={userEdit.name}
+                                                            disabled={edit}
+                                                            className=" border-gray-400 border-[1px] rounded-sm px-5 py-2 focus:outline-none w-full"
+                                                            onChange={handleChange}
+                                                            name="name"
+                                                            placeholder="Name"
+                                                        />
+                                                        <input
+                                                            type="text"
+                                                            value={userEdit.email}
+                                                            disabled
+                                                            className=" border-gray-400 text-gray-400 font-light cursor-not-allowed border-[1px] rounded-sm px-5 py-2 focus:outline-none w-full"
+                                                            onChange={handleChange}
+                                                            name="email"
+                                                            placeholder="Email"
+                                                        />
+                                                        <input
+                                                            type="text"
+                                                            value={userEdit.phone}
+                                                            disabled
+                                                            className=" border-gray-400 cursor-not-allowed border-[1px] rounded-sm px-5 py-2 focus:outline-none w-full"
+                                                            onChange={handleChange}
+                                                            name="phone"
+                                                            placeholder="Phone"
+                                                        />
+
+
+                                                        
+
+                                                        <button
+                                                            type="submit"
+                                                            className="bg-blue-500 rounded-sm py-2 px-5 text-white"
+                                                            onClick={handleProfileUpdate}
+                                                        >
+                                                            Update Profile
+                                                        </button>
+                                                    </form>
+                                                </div>
+                                            </div>
+
+                                            <div className="w-1/2">
+
+                                                <div className="flex justify-between items-center">
+                                                    <h2 className="text-lg font-medium">Change Password</h2>
+
+                                                    <FiLock className="text-gray-500 w-6 h-6" />
+                                                </div>
+
+                                                <div className="">
+                                                    <form className={`flex flex-col gap-4 mt-6`}>
+
+
+                                                        <div>
+                                                            <input
+                                                                type="password"
+                                                                value={updatedPassword.newPassword}
+                                                                className="border-gray-400 border-[1px] rounded-sm px-5 py-2 focus:outline-none w-full"
+                                                                onChange={handlePasswordChange}
+                                                                name="newPassword"
+                                                                placeholder="New Password"
+                                                            />
+
+                                                        </div>
+
+                                                        <div className="relative">
+                                                            <input
+                                                                type={showPassword ? "text" : "password"}
+                                                                value={updatedPassword.confirmPassword}
+                                                                className="border-gray-400 border-[1px] rounded-sm px-5 py-2 focus:outline-none w-full"
+                                                                onChange={handlePasswordChange}
+                                                                name="confirmPassword"
+                                                                placeholder="Confirm Password"
+                                                            />
+
+
+                                                            <div onClick={() => setShowPassword(!showPassword)}>
+                                                                {showPassword ? (
+                                                                    <FiEye
+                                                                        strokeWidth={1.5}
+                                                                        className="bx bx-hide eye-icon absolute top-1/2 transform -translate-y-1/2 right-0 mr-4 text-sm cursor-pointer text-gray-400 w-6 h-6"
+                                                                    />
+                                                                ) : (
+                                                                    <FiEyeOff
+                                                                        strokeWidth={1.5}
+                                                                        className="bx bx-show eye-icon absolute top-1/2 transform -translate-y-1/2 right-0 mr-4 text-sm cursor-pointer text-gray-400 w-6 h-6"
+                                                                    />
+                                                                )}
+                                                            </div>
+
+
+                                                        </div>
+
+
+
+                                                        {updatedPassword.confirmPassword && (updatedPassword.newPassword !== updatedPassword.confirmPassword ? (
+                                                            <p className="text-red-500 text-xs italic">
+                                                                Passwords do not match
+                                                            </p>
+                                                        ) : (
+                                                            <p className="text-green-500 text-xs italic">
+                                                                Passwords Match
+                                                            </p>
+                                                        )
+                                                        )}
+
+                                                            <button type="submit" className={`bg-blue-500 rounded-sm py-2 px-5 text-white ${!updatedPassword.confirmPassword || updatedPassword.newPassword !== updatedPassword.confirmPassword ? "cursor-not-allowed" : ""}`} disabled={!updatedPassword.confirmPassword || updatedPassword.newPassword !== updatedPassword.confirmPassword} onClick={handlePasswordClick}>
+                                                                Change Password
+                                                            </button>
+
+                                                    </form>
+                                                </div>
+
+                                            </div>
+
                                         </div>
 
-                                        <div className="">
-                                            <form className={`flex flex-col gap-2 mt-6`}>
 
-                                                <input
-                                                    type="text"
-                                                    value={userEdit.name}
-                                                    disabled={edit}
-                                                    className=" border-gray-400 border-[1px] rounded-sm px-5 py-2 focus:outline-none w-full"
-                                                    onChange={handleChange}
-                                                    name="name"
-                                                />
-                                                <input
-                                                    type="text"
-                                                    value={userEdit.email}
-                                                    disabled={edit}
-                                                    className=" border-gray-400 border-[1px] rounded-sm px-5 py-2 focus:outline-none w-full"
-                                                    onChange={handleChange}
-                                                    name="email"
-                                                />
-                                                <input
-                                                    type="text"
-                                                    value={userEdit.phone}
-                                                    disabled={edit}
-                                                    className=" border-gray-400 border-[1px] rounded-sm px-5 py-2 focus:outline-none w-full"
-                                                    onChange={handleChange}
-                                                    name="phone"
-                                                />
 
-                                                <button
-                                                    type="submit"
-                                                    className="bg-blue-500 rounded-sm py-2 px-5 text-white"
-                                                >
-                                                    Update Business
-                                                </button>
-                                            </form>
-                                        </div>
                                     </div>
                                 )}
                                 {selectedField === "allListings" && <AllListings />}
@@ -429,8 +573,10 @@ const VendorDashboard = () => {
                         )}
                     </div>
                 </div>
-            </div>
-        </div>
+            </div >
+            <ToastContainer />
+
+        </div >
     );
 };
 
