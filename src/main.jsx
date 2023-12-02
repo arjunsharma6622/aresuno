dcdb064d41b7de8fa3a7049d129d2385980e0ffb
Arjun Sharma <arjunsharma6622@gmail.com>
2023-12-02 23:56:10 +0530

    made the entire code modular, clean

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from "react-redux"
-import { store, persistor } from './state/store.js'
+import { store, persistor } from './state/Store.js'
 import { PersistGate } from 'redux-persist/integration/react'
 
 
