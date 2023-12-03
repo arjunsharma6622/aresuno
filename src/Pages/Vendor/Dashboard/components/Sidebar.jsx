589fb3b6bf9bc753cc2e22057007e7108f02d9d0
Arjun Sharma <arjunsharma6622@gmail.com>
2023-12-04 01:00:09 +0530

    vendor dashboard sidebar design updated

--- src/Pages/Vendor/Dashboard/components/Sidebar.jsx
+++ src/Pages/Vendor/Dashboard/components/Sidebar.jsx
@@ -14,12 +14,15 @@ import {
 } from "react-icons/md";
 import { useDispatch } from "react-redux";
 import { userLogout } from "../../../../state/slices/userSlice";
-import { FiArrowLeft, FiHome } from "react-icons/fi";
+import { FiArrowLeft, FiArrowRight, FiLogOut, FiStar } from "react-icons/fi";
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
 
 const Sidebar = ({ user }) => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const location = useLocation()
+  const location = useLocation();
+
+  const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(false);
 
   const sidebarLinks = [
     {
@@ -49,7 +52,7 @@ const Sidebar = ({ user }) => {
     },
     {
       name: "Reviews",
-      icon: <MdOutlineReviews className="w-6 h-6 " />,
+      icon: <FiStar className="w-6 h-6 " />,
       path: "reviews",
     },
     {
@@ -66,72 +69,93 @@ const Sidebar = ({ user }) => {
 
   const isActiveLink = (path) => {
     if (path === "") {
-      return location.pathname === `/vendor/dashboard/` || location.pathname === `/vendor/dashboard`;
+      return (
+        location.pathname === `/vendor/dashboard/` ||
+        location.pathname === `/vendor/dashboard`
+      );
     }
     return location.pathname === `/vendor/dashboard/${path}`;
-  }
+  };
+
+  const toggleSidebar = () => {
+    setIsSidebarCollapsed(!isSidebarCollapsed);
+  };
 
   return (
-    <div className="flex-[2] overflow-y-auto border-r border-gray-300 p-10 flex flex-col justify-between items-start">
+    <div
+      className={` relative overflow-y-auto p-4  shadow-xl flex flex-col justify-between items-start transition-all duration-300 ${isSidebarCollapsed ? " w-28 p-2" : "w-64"
+        }`}
+    >
       <div className="flex flex-col gap-8 w-full">
-        
         <div className="flex flex-col items-start justify-start gap-6 w-full">
 
-      <Link to="/">
-            <div className="flex items-center cursor-pointer gap-2">
-              <FiArrowLeft className="w-5 h-5" />
-              <span className="text-base">Home</span>
-            </div>
-          </Link>
-        <div className="flex items-center gap-4">
-          <img
-            src={
-              user.image
-                ? user.image
-                : "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
-            }
-            alt=""
-            className="rounded-full w-10 h-10 object-cover"
-          />
-          <h2 className="text-2xl font-semibold">{user.name}</h2>
+
+          <div className="absolute right-0 transform top-6 z-[10] flex items-center cursor-pointer gap-2">
+            {!isSidebarCollapsed && (
+              <div className="bg-gray-300 p-1 rounded-l-lg flex items-center gap-1" onClick={toggleSidebar}>
+                <FaAngleLeft className="w-5 h-5 text-gray-800" />
+                {/* <span>Hide</span> */}
+              </div>
+            )}
+
+
+            {isSidebarCollapsed && (
+              <div className="bg-gray-300 p-1 rounded-l-lg flex items-center gap-1" onClick={toggleSidebar}>
+                {/* <span>Open</span> */}
+                <FaAngleRight className="w-5 h-5" />
+              </div>
+            )}
+          </div>
+
+
+          <div className="flex items-center gap-4">
+            <img
+              src={
+                user.image
+                  ? user.image
+                  : "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+              }
+              alt=""
+              className="rounded-full w-10 h-10 object-cover"
+            />
+            {!isSidebarCollapsed && (
+              <h2 className="text-2xl font-semibold">{user.name}</h2>
+            )}
+          </div>
         </div>
-
-        </div>
-
-
-
 
         <hr className="w-full border-gray-300" />
 
-        <div className="text-sm flex flex-col gap-8 w-full">
-
-
+        <div className={` flex flex-col ${isSidebarCollapsed ? "items-center" : "items-start"} gap-4 w-full`}>
           {sidebarLinks.map(({ name, icon, path }, index) => (
-            <Link to={path} key={index}>
+            <Link to={path} key={index} className={`w-full rounded-md p-2 px-4 ${isActiveLink(path) ? "bg-blue-500" : ""}`} >
               <div
-                className={`flex items-center cursor-pointer gap-2 ${
-                  isActiveLink(path) ? "text-blue-500" : "text-gray-700"
-                }`}
+                className={`flex items-center ${isSidebarCollapsed ? "justify-center" : "justify-start"} w-full} cursor-pointer gap-2 ${isActiveLink(path) ? "text-white" : "text-black-500"
+                  }`}
               >
                 {icon}
-                <span className="">{name}</span>
+                { !isSidebarCollapsed &&
+                <span className="text-base">{name}</span>
+}
               </div>
             </Link>
           ))}
         </div>
       </div>
 
+      <hr className="w-full border-gray-300" />
+
       <div className="w-full">
-        <button
-          className="w-full px-4 py-1 border text-red-500 border-red-500"
-          onClick={() => {
-            localStorage.removeItem("token");
-            dispatch(userLogout());
-            navigate("/");
-          }}
-        >
-          Logout
-        </button>
+          <button
+            className={`w-full flex items-center justify-center px-4 py-2 border text-white border-red-500 bg-red-500 rounded-lg ${isSidebarCollapsed && "border-0"}`}
+            onClick={() => {
+              localStorage.removeItem("token");
+              dispatch(userLogout());
+              navigate("/");
+            }}
+          >
+            {isSidebarCollapsed ? <FiLogOut className="w-7 h-7"/> : "Logout"}
+          </button>
       </div>
     </div>
   );
