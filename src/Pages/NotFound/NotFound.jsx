268cb86b0c65c87e16a8ba9c22e6b14bfd23d8dd
Arjun Sharma <arjunsharma6622@gmail.com>
2023-12-29 14:11:19 +0530

    added searchbar feature in home page

--- src/Pages/NotFound/NotFound.jsx
+++ src/Pages/NotFound/NotFound.jsx
@@ -5,13 +5,13 @@ import { Link } from 'react-router-dom';
 const NotFound = () => {
   return (
     <div className="flex flex-col items-center w-full justify-center h-[90vh] bg-gray-100">
-      <div className=" w-[50%] text-center flex flex-col gap-6 items-start ">
+      <div className="px-4 md:w-[50%] text-center flex flex-col gap-6 items-start ">
         <img src="./assets/error.png" alt="" className='w-32'/>
 
         <div className='flex flex-col items-start'>
-        <h1 className=" text-6xl font-semibold text-red-500 mb-4">404</h1>
+        <h1 className=" text-4xl md:text-6xl font-semibold text-red-500 mb-4">404</h1>
         <p className="text-2xl mb-4">Oops! Page not found.</p>
-        <p className="text-gray-500 mb-4">The page you are looking for does not exist or might have been removed.</p>
+        <p className="text-gray-500 mb-4 text-start">The page you are looking for does not exist or might have been removed.</p>
         </div>
 
 
