4706de9f0163ab7893d175f465d4a8452a9cf008
Arjun Sharma <arjunsharma6622@gmail.com>
2024-01-22 04:07:00 +0530

    enquiry form updated

--- src/Components/EnquiryForm.jsx
+++ src/Components/EnquiryForm.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { FiX } from 'react-icons/fi';
+import { FiCheckCircle, FiMessageSquare, FiX } from 'react-icons/fi';
 import { API_URL } from '../utils/util';
 import axios from 'axios'
+import { ToastContainer, toast } from 'react-toastify';
 
 const EnquiryForm = ({ onClose, business, categoryId }) => {
   const [enquiry, setEnquiry] = useState({
@@ -10,9 +11,13 @@ const EnquiryForm = ({ onClose, business, categoryId }) => {
     message: '',
     category: categoryId,
   });
+  const [isSending, setIsSending] = useState(false);
+  const [isSent, setIsSent] = useState(false);
 
   const handleEnquirySubmit = async (e) => {
     e.preventDefault();
+
+    setIsSending(true);
 
     try {
         const enquiryToSend = {
@@ -23,8 +28,12 @@ const EnquiryForm = ({ onClose, business, categoryId }) => {
         }
         const res = await axios.post(`${API_URL}/api/enquiry/create`, enquiryToSend)
         console.log(res)
-        onClose();
+        setIsSending(false)
+        setIsSent(true)
+        toast.success('Enquiry Sent')
+        
     } catch (err) {
+        setIsSending(false)
       console.error(err);
     }
   };
@@ -39,73 +48,100 @@ const EnquiryForm = ({ onClose, business, categoryId }) => {
   return (
     <div>
       <div className='z-[40] fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-50 backdrop-filter backdrop-blur-sm'>
-        <div className='relative w-[90%] md:w-[70%] bg-white flex gap-6  rounded-xl'>
+        { !isSent ? (
+                <div className='flex flex-col md:flex-row relative w-[90%] md:w-[70%] bg-white  gap-6  rounded-xl'>
 
-          <div className='flex-[7] p-8 flex flex-col gap-6 '>
+                <div className='flex-[7] p-8 md:px-12 flex flex-col gap-6 '>
+                  <div>
+                    <h1 className='text-2xl font-semibold'><FiMessageSquare className='inline w-8 h-8 mr-2' /> Enquiry</h1>
+                    <p className='text-gray-600 text-sm md:text-base mt-2'>Fill out the form below to get in touch with us for more details.</p>
+                  </div>
+      
+                <form
+                  onSubmit={handleEnquirySubmit}
+                  className='flex flex-col gap-4'
+                >
+                  <div className='flex flex-col gap-1'>
+                    <label htmlFor='name'>Name</label>
+                    <input
+                      type='text'
+                      id='name'
+                      name='name'
+                      value={enquiry.name}
+                      className='rounded-md border text-base border-gray-300 w-full py-3 px-4 text-gray-600 focus:outline-none focus:border-blue-500'
+                      onChange={(e) => handleFormChange(e)}
+                    />
+                  </div>
+                  <div className='flex flex-col gap-1'>
+                    <label htmlFor='phone'>Phone</label>
+                    <input
+                      type='number'
+                      id='phone'
+                      name='phone'
+                      value={enquiry.phone}
+                      className='rounded-md border text-base border-gray-300 w-full py-3 px-4 text-gray-600 focus:outline-none focus:border-blue-500'
+                      onChange={(e) => handleFormChange(e)}
+                    />
+                  </div>
+                  <div className='flex flex-col gap-1'>
+                    <label htmlFor='message'>
+                      Message <span className='text-sm'>(optional)</span>
+                    </label>
+                    <input
+                      type='text'
+                      id='message'
+                      name='message'
+                      value={enquiry.message}
+                      className='rounded-md border text-base border-gray-300 w-full py-3 px-4 text-gray-600 focus:outline-none focus:border-blue-500'
+                      onChange={(e) => handleFormChange(e)}
+                    />
+                  </div>
+                  <button
+                    type='submit'
+                    className='bg-blue-500 text-white px-4 py-2 rounded-md'
+                  >
+                    {
+                      isSending ? 'Sending...' : 'Send Enquiry'
+                    }
+                  </button>
+                </form>
+                </div>
+      
+                {/* <div className='flex flex-[5] bg-blue-100 rounded-xl pl-4'>
+                  <img src="/assets/form.png" alt="" className='rounded-xl object-cover'/>
+                </div> */}
+      
+      
+      
+                <div className='absolute top-4 right-4'>
+                    <FiX className='w-5 h-5 cursor-pointer' onClick={onClose} />
+                  </div>
+              </div>  
+        ) : (
+          <div className='w-[90%] md:w-[40%] bg-white  flex gap-6  rounded-xl'>
+
+          <div className='flex-[7] p-8 md:px-12 flex flex-col gap-6 '>
             <div>
-              <h1 className='text-xl font-semibold'>Enquiry</h1>
-              <p>We will get back to you in some time</p>
+              <div className='flex justify-between'>
+              <h1 className='text-2xl font-semibold'><FiCheckCircle className='text-green-500 inline w-8 h-8 mr-2' /> Submitted</h1>
+              <FiX className='w-5 h-5 cursor-pointer' onClick={() => {
+                                onClose()
+
+                setIsSent(false)
+              }} />
+              </div>
+              <p className='text-gray-600 text-sm md:text-base mt-2'>We have received your enquiry, we will get back to you shortly</p>
+              <p className='text-gray-600 text-sm md:text-base mt-2'>Thank you!</p>
             </div>
 
-          <form
-            onSubmit={handleEnquirySubmit}
-            className='flex flex-col gap-4'
-          >
-            <div className='flex flex-col gap-1'>
-              <label htmlFor='name'>Name</label>
-              <input
-                type='text'
-                id='name'
-                name='name'
-                value={enquiry.name}
-                className='rounded-md border text-base border-gray-300 w-full py-3 px-4 text-gray-600 focus:outline-none focus:border-blue-500'
-                onChange={(e) => handleFormChange(e)}
-              />
-            </div>
-            <div className='flex flex-col gap-1'>
-              <label htmlFor='phone'>Phone</label>
-              <input
-                type='number'
-                id='phone'
-                name='phone'
-                value={enquiry.phone}
-                className='rounded-md border text-base border-gray-300 w-full py-3 px-4 text-gray-600 focus:outline-none focus:border-blue-500'
-                onChange={(e) => handleFormChange(e)}
-              />
-            </div>
-            <div className='flex flex-col gap-1'>
-              <label htmlFor='message'>
-                Message <span className='text-sm'>(optional)</span>
-              </label>
-              <input
-                type='text'
-                id='message'
-                name='message'
-                value={enquiry.message}
-                className='rounded-md border text-base border-gray-300 w-full py-3 px-4 text-gray-600 focus:outline-none focus:border-blue-500'
-                onChange={(e) => handleFormChange(e)}
-              />
-            </div>
-            <button
-              type='submit'
-              className='bg-blue-500 text-white px-4 py-2 rounded-md'
-            >
-              Submit Enquiry
-            </button>
-          </form>
+                  </div>
           </div>
-
-          <div className='flex flex-[5]'>
-            <img src="https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/10/free-images.jpg" alt="" className='rounded-xl object-cover'/>
-          </div>
+        )
 
 
-
-          <div className='absolute top-4 right-4'>
-              <FiX className='w-5 h-5 cursor-pointer' onClick={onClose} />
-            </div>
-        </div>
+}
       </div>
+      <ToastContainer />
     </div>
   );
 };
