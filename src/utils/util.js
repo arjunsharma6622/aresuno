4d2fc9ed008dae006e4bf2a065cc6006c5a6a084
Prashant <prashantrahul141@protonmail.com>
2024-03-30 19:02:02 +0530

    add validate functions to utils

--- src/utils/util.js
+++ src/utils/util.js
@@ -14,3 +14,37 @@ export const ToastParams = {
   theme: "light",
   closeButton: false,
 };
+
+/**
+ * Validates phone number using regex.
+ * @param  {string} phoneNumber
+ * @return {boolean}
+ */
+export const validatePhoneNumber = (phoneNumber) => {
+  const phoneNumberRegex =
+    /^(\+\d{1,2}\s?)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/gm;
+
+  return phoneNumberRegex.test(phoneNumber);
+};
+
+/**
+ * Validates password using regex.
+ * @param  {string} password
+ * @return {boolean}
+ */
+export const validatePassword = (password) => {
+  const passwordRegex =
+    /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
+
+  return passwordRegex.test(password);
+};
+
+/**
+ * Validates email address using regex.
+ * @param  {string} email
+ * @return {boolean}
+ */
+export const validateEmailAddress = (email) => {
+  const emailRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/;
+  return emailRegex.test(email);
+};
