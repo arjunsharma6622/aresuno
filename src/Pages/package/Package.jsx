16e22a079299c78f23351f998dc0beae6fa2c18d
Prashant <prashantrahul141@protonmail.com>
2024-04-11 14:32:16 +0530

    fix: large space between header and packagetop content in mobile view

--- src/Pages/package/Package.jsx
+++ src/Pages/package/Package.jsx
@@ -4,7 +4,7 @@ import PackageHead from "./components/PackageHead";
 const PackageSection = () => {
   return (
     <div className="bg-white flex items-center justify-center">
-      <div className="w-[90%] mx-auto flex flex-col gap-32 mt-20">
+      <div className="w-[90%] mx-auto flex flex-col gap-32 mt-6 md:mt-16">
         <PackageHead />
         <Pricing />
       </div>
