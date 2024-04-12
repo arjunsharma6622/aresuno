74c1edc15a875d9077a8d68ff191d797eb278b0b
Prashant <prashantrahul141@protonmail.com>
2024-04-12 16:33:34 +0530

    use headcomponent in packages section

--- src/Pages/package/Package.jsx
+++ src/Pages/package/Package.jsx
@@ -1,14 +1,18 @@
 import Pricing from "./components/Pricing";
 import PackageHead from "./components/PackageHead";
+import HeadComponent from "../../Components/Common/HeadComponent";
 
 const PackageSection = () => {
   return (
-    <div className="bg-white flex items-center justify-center">
-      <div className="w-[90%] mx-auto flex flex-col gap-32 mt-6 md:mt-16">
-        <PackageHead />
-        <Pricing />
+    <>
+      <HeadComponent _title={"Packages"}></HeadComponent>
+      <div className="bg-white flex items-center justify-center">
+        <div className="w-[90%] mx-auto flex flex-col gap-32 mt-6 md:mt-16">
+          <PackageHead />
+          <Pricing />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
