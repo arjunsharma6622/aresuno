e50d27a29ffdd82b3868a597b05979be2d12f9db
Prashant <prashantrahul141@protonmail.com>
2024-04-18 21:06:55 +0530

    make detailed comparison responsive for desktops

--- src/Pages/package/components/DetailedComparison.jsx
+++ src/Pages/package/components/DetailedComparison.jsx
@@ -7,19 +7,21 @@ const DetailedComparison = ({ packagesData }) => {
   );
 
   return (
-    <div className="flex w-[100vw] ml-4">
+    <div className="flex w-[100vw] lg:w-min ml-4">
       {/* features side */}
-      <div className="bg-white min-w-36">
+      <div className="bg-white min-w-36 lg:min-w-52">
         <div className="py-[.38rem]">
-          <p className="font-bold uppercase pl-2">Features</p>
+          <p className="font-bold uppercase pl-3 text-base lg:text-lg">
+            Features
+          </p>
         </div>
 
         {features.map((feature) => (
           <div
             key={feature}
-            className="py-[.86rem] pl-2 rounded-sm odd:bg-primary-salmon-500"
+            className="py-[.86rem] lg:py-5 pl-3 rounded-sm lg:rounded-l-2xl odd:bg-primary-salmon-500"
           >
-            <p className="text-xs tracking-wide">{feature}</p>
+            <p className="text-xs tracking-wide lg:text-sm">{feature}</p>
           </div>
         ))}
       </div>
@@ -29,10 +31,12 @@ const DetailedComparison = ({ packagesData }) => {
         {/* mapping through packages */}
         {packagesData.map((pkg) => {
           return (
-            <div key={pkg._id} className="min-w-32">
+            <div key={pkg._id} className="min-w-32 md:min-w-36 lg:min-w-56">
               {/* package name */}
-              <div className="flex items-center py-2 justify-center">
-                <p className="font-bold uppercase text-sm">{pkg.name}</p>
+              <div className="flex items-center py-2 lg:py-[.4rem] justify-center">
+                <p className="font-bold uppercase text-sm lg:text-lg">
+                  {pkg.name}
+                </p>
               </div>
 
               {/* mapping through features */}
@@ -40,10 +44,10 @@ const DetailedComparison = ({ packagesData }) => {
                 return (
                   <div
                     key={feature}
-                    className="flex items-center py-[.925rem] odd:bg-neutral-100 justify-center"
+                    className="flex items-center py-[.925rem] lg:py-[1.31rem] odd:bg-neutral-100 justify-center"
                   >
                     {/* state of the feature */}
-                    <p className="font-normal text-sm">
+                    <p className="font-normal text-sm lg:text-lg">
                       {pkg.features.includes(feature) ? (
                         <FaCheckCircle className="text-primary-allowing-green" />
                       ) : (
